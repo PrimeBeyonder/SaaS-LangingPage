25da434a945615908bc026c40f32561fb6fd5c57
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-01-13 22:03:20 +0630

    Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,40 @@
-export const Hero = () => {
-  return null;
+import ArrowRight from "@/assets/arrow-right.svg"
+import cogImage from "@/assets/cog.png";
+import Image from "next/image";
+import cylinder from "@/assets/cylinder.png";
+import noodleImage from "@/assets/noodle.png";
+
+const Hero = () => {
+  return (
+      <section className="pt-8 pb-20 md:pt-5 md:pb-10 overflow-x-clip"
+               style={{background: 'radial-gradient(ellipse 200% 100% at bottom left, #183EC2, #EAEEFE 86%)'}}>
+          <div className="container">
+              <div className="md:flex items-center">
+                  <div className="md:w-[478px]">
+                      <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg"> Version 2.0 Is
+                          Here
+                      </div>
+                      <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] bg-clip-text mt-6 text-transparent">Pathway
+                          To Productivity</h1>
+                      <p className="text-xl text-[#010D3E] tracking-tight mt-6 ">Celebrate the joy of accomplishment with an
+                          app designed to track your progress and motivate your efforts.</p>
+                      <div className="flex gap-2 items-center mt-[30px]">
+                          <button className="btn btn-primary">Get For Free</button>
+                          <button className="btn btn-text gap-2">
+                              <span>Learn More</span>
+                              <ArrowRight className="h-5 w-5"/>
+                          </button>
+                      </div>
+                  </div>
+                  <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+                      <Image src={cogImage} alt="Cog Image" className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"/>
+                      <Image src={cylinder} alt="" width={220} height={220} className="hidden md:block -top-8 -left-32 md:absolute"/>
+                      <Image src={noodleImage} alt="" width={220} height={220} className="hidden lg:block rotate-[30%] top-[524px] left-[448px] md:absolute"/>
+                  </div>
+              </div>
+          </div>
+      </section>
+  );
 };
+
+export default Hero;
