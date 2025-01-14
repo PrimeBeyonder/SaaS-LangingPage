beb84b06d8d91927e4044d1cef6867c32a680130
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-01-14 20:35:31 +0630

    logo improt and render

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,22 @@
+import acmeLogo from "@/assets/logo-acme.png"
+import quantumLogo from "@/assets/logo-quantum.png"
+import echoLogo from "@/assets/logo-echo.png"
+import celestialLogo from "@/assets/logo-celestial.png"
+import pulseLogo from "@/assets/logo-pulse.png"
+import apexLogo from "@/assets/logo-apex.png"
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+      <div className="container">
+          <div>
+            <Image src={acmeLogo} alt="Acme Logo"/>
+            <Image src={quantumLogo} alt="quantumLogo"/>
+            <Image src={echoLogo} alt="Acme Logo"/>
+            <Image src={celestialLogo} alt="Acme Logo"/>
+            <Image src={pulseLogo} alt="Acme Logo"/>
+            <Image src={apexLogo} alt="Acme Logo"/>
+          </div>
+      </div>
+  );
 };
