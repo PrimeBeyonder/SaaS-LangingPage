6649cd3fde6cb1c32e2839721af354aaf67703fd
Arnold <149857208+PrimeBeyonder@users.noreply.github.com>
2025-01-15 21:24:52 +0630

    format for product image section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import productImage from "@/assets/product-image.png"
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section>
+      <div className="container">
+        <div >Boots Your Productivity</div>
+        <h2>A More Effective Way To Track Progess</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium nesciunt magni ad esse, quaerat velit voluptates sequi maiores aspernatur, voluptatem iure autem repellendus laboriosam sunt dignissimos, optio molestiae error odio?
+        </p>
+        <Image/>
+      </div>
+    </section>
+  );
 };
