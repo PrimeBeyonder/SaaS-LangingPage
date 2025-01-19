971ac923fbbf319ac5460cabeedc098724125c5c
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2025-01-19 18:24:55 +0630

    logo loop animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,10 +1,12 @@
-import acmeLogo from "@/assets/logo-acme.png"
-import quantumLogo from "@/assets/logo-quantum.png"
-import echoLogo from "@/assets/logo-echo.png"
-import celestialLogo from "@/assets/logo-celestial.png"
-import pulseLogo from "@/assets/logo-pulse.png"
-import apexLogo from "@/assets/logo-apex.png"
+"use client";
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
   return (
@@ -14,18 +16,85 @@ export const LogoTicker = () => {
           <div
             className="flex overflow-hidden"
             style={{
-              maskImage: 'linear-gradient(to right, transparent, black 25%, black 75%, transparent)',
-              WebkitMaskImage: 'linear-gradient(to right, transparent, black 25%, black 75%, transparent)', // For Safari support
+              maskImage:
+                "linear-gradient(to right, transparent, black 25%, black 75%, transparent)",
+              WebkitMaskImage:
+                "linear-gradient(to right, transparent, black 25%, black 75%, transparent)", // For Safari support
             }}
           >
-            <div className="flex gap-14 flex-none">
-              <Image className="logo-ticker-image" src={acmeLogo} alt="Acme Logo" />
-              <Image className="logo-ticker-image" src={quantumLogo} alt="quantumLogo" />
-              <Image className="logo-ticker-image" src={echoLogo} alt="Acme Logo" />
-              <Image className="logo-ticker-image" src={celestialLogo} alt="Acme Logo" />
-              <Image className="logo-ticker-image" src={pulseLogo} alt="Acme Logo" />
-              <Image className="logo-ticker-image" src={apexLogo} alt="Acme Logo" />
-            </div>
+            <motion.div
+              className="flex gap-14 flex-none pr-14"
+              animate={{
+                translateX: "-50%",
+              }}
+              transition={{
+                duration: 10,
+                repeat: Infinity,
+                ease: "linear",
+                repeatType: "loop",
+              }}
+            >
+              <Image
+                className="logo-ticker-image"
+                src={acmeLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={quantumLogo}
+                alt="quantumLogo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={echoLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={celestialLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={pulseLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={apexLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={acmeLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={quantumLogo}
+                alt="quantumLogo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={echoLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={celestialLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={pulseLogo}
+                alt="Acme Logo"
+              />
+              <Image
+                className="logo-ticker-image"
+                src={apexLogo}
+                alt="Acme Logo"
+              />
+            </motion.div>
           </div>
         </div>
       </div>
